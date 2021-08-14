3fa6b2845391468603042a9804f644f5e9ae09c5
min33sky <min33sky@naver.com>
2021-08-14 13:27:10 +0900

    auth start

--- postgre-sql-practice/src/app.module.ts
+++ postgre-sql-practice/src/app.module.ts
@@ -4,12 +4,13 @@ import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(), //* TypeORM이 root에 있는 ormconfig.js를 우선적으로 읽어들인다.
-    BoardsModule,
+    BoardsModule, AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
